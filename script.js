99fdec315dffb34a4294a806a80c06e35c34918b
Rafael Epifanio <rafael.cepifanio@gmail.com>
2019-12-02 22:22:47 -0300

    Exercicio 3 concluido

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 * Exercicio número 1 do desafio 1.
 * @uthor: Rafael Epifanio  
 * Data: 02/12/2019
+*/
 var endereco = {
     rua: "Beco do binga",
     numero: 105,
@@ -12,18 +13,16 @@ var endereco = {
 
 console.log("O Rafael mora na rua" + endereco.rua + " " + endereco.numero + ", Bairro:" 
             + endereco.bairro + ", Cidade:" + endereco.cidade + "/" + endereco.uf); 
-*/
-
-
 /*
-* Exercicio númeroo 3 do desafio 1
+* Exercicio númeroo 2 do desafio 1
 * @uthor: Rafaek Epifanio
 * Data: 02/12/2019
+*/
 var inputElement1 = document.getElementById("numeroField1");
 var inputElement2 = document.getElementById("numeroField2");
-var buttonElemnte = document.getElementById("btMostra");
+var buttonElementExec1 = document.getElementById("btExercicio2");
 
-buttonElemnte.onclick = function(){
+buttonElementExec1.onclick = function(){
     
     var i = inputElement1.value;
 
@@ -32,7 +31,26 @@ buttonElemnte.onclick = function(){
              console.log(i);
          }
         i++;
-
     }
 }
+/*
+* Exercico número 3 do desafio 1
+* @uthor: Rafael Epifanio
+* Data: 02/12/2019
 */
+var skills = ["java", "javaScript", "C#", "PHP", "react"];
+
+var buttonElementExec2 = document.getElementById("btExercicio3");
+var js = skills.indexOf("javaScript");
+
+buttonElementExec2.onclick = function (){
+    if (js >= 0){
+        alert("Encontrado na posição:" + js);
+    }   
+}
+/*
+* Exercico número 4 do desafio 1
+* @uthor: Rafael Epifanio
+* Data: 02/12/2019
+*/
+
